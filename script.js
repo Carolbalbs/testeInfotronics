a07f94728ad977914bec79f5a7c2150a4621e6c1
carolbalbs <carolbalbinos@hotmail.com>
2023-12-28 08:57:14 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const ordenados = Lista-numros-ordenados;
-
-const conteudoJson = '{';
-    conteudoJson += '"numero": "'+ conteudoArray
-conteudoJson += '}';
-
-
-let arquivo = new Arquivo [conteudoJson],{type:"text/plain;charset=utf-8"}
-saveAs(blo, ordenados+".json")
